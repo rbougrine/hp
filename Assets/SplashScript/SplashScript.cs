5183d249a5f79e99c11e2859990d2bf5e9e07fa4
AnnyA95 <anni032@hotmail.com>
2016-05-06 11:51:35 +0200

    Revert "Revert "Splash screen added + Vr sulution""
    This reverts commit deed7f1ac5f4e56369fb372810c17906d63e89c5.

---
+++ Assets/SplashScript/SplashScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScript : MonoBehaviour
+{
+
+    public float loadingTime;
+    public Image loadingBar;
+    public Text percent;
+
+    void Start()
+    {
+
+        loadingBar.fillAmount = 0;
+
+    }
+
+
+    void Update()
+    {
+
+        if (loadingBar.fillAmount <= 1)
+        {
+            loadingBar.fillAmount += 1.0f / loadingTime * Time.deltaTime;
+        }
+        if (loadingBar.fillAmount == 1.0f)
+        {
+			SceneManager.LoadScene ("Login");
+			Debug.Log ("splash done get to loginscreen");
+        }
+        percent.text = (loadingBar.fillAmount * 100).ToString("f0");
+    }
+}
